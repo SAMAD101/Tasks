690bed1429cdfb84c037914f8869d3be5e8c4d07
Sam <asamadans@gmail.com>
2023-02-16 00:08:27 +0530

    cleaned unnecessary lines

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django import forms
 # Create your views here.
